0416a7d154c428d9da7ef8de7ae66bfcbaf011b5
anshu-goyal-lf <anshu.goyal@loanframe.com>
2018-02-25 18:40:29 +0530

    Database config added

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,41 +1,107 @@
+
+// OauthServer
+const OAuth2Server       = require('oauth2-server');
+const AccessDeniedError  = require('oauth2-server/lib/errors/access-denied-error');
+const Request            = OAuth2Server.Request;
+const Response           = OAuth2Server.Response;
+
 // routes/note_routes.js
-var User = require('./../models/user');
+var OAuthUsersModel = require('./../models/oauthusers');
+
+
+const oauth = new OAuth2Server({
+  model: require('./../models/model.js'),
+  grants: ['password'],
+  debug: true
+});
+
 
 module.exports = function(app, db) {
 
-  app.post('/api/signup', (req, res) => {
+  app.post('/api/login', (req, res) => {
 
-    var newUser = new User({
-      name: req.body.name,
-      username: req.body.username,
-      password: req.body.password,
+    let response = new Response({
+        headers: { }
     });
 
-    newUser.save(function(err) {
-      if (err) {
-        res.send('Sorry, User already present');
-      }
-      else
-      res.send({ status: 200, message: 'Registration Successfully' });
+    let request = new Request({
+        method: 'POST',
+        query: {},
+        headers: {},
+        body: {
+            client_id: 'LoanTrack',
+            client_secret: '123',
+            grant_type: 'password',
+            username: req.body.username,
+            password: req.body.password,
+        }
     });
+
+    let options = {
+      requireClientAuthentication: { password: false },
+      accessTokenLifetime: 3600
+    };
+
+    oauth.token(request, response, options)
+      .then((token) => {
+        // The resource owner granted the access request.
+        res.send(token)
+      })
+      .catch((err) => {
+        // The request was invalid or not authorized.
+        console.log(err);
+        res.send('User not registered!');
+      });
 
   });
 
-  app.post('/api/login', (req, res) => {
+  app.post('/api/signup', (req, res) => {
 
-    User.find(
-      {
-        username: req.body.username,
-        password: req.body.password
-      },
-      function(err, user) {
+      var newUser = OAuthUsersModel({
+        email:  req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: req.body.password,
+        username: req.body.username
+      });
+
+      newUser.save(function(err, user) {
         if (err) {
-          res.send('User not registered!');
+            res.send('Error in registration!');
         }
         else
-        res.send(user);
-      }
-    );
+            res.send(user);
+      });
+
+  });
+
+  app.post('/api/getUser', (req, res) => {
+
+     let response = new Response({
+            headers: { }
+        });
+
+    let request = new Request(req);
+
+    oauth.authenticate(request, response)
+      .then((token) => {
+
+        OAuthUsersModel.find({
+          username: req.body.username,
+          password: req.body.password
+        },function(err, user) {
+          if (err) {
+              res.send('No user found!');
+          }
+          else
+              res.send(user);
+        });
+
+      })
+      .catch((err) => {
+        res.send('Invalid Token')
+      });
+
 
   });
 
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 // server.js
-const express        = require('express');
-const mongoose       = require('mongoose');
-const bodyParser     = require('body-parser');
-const app            = express();
-const db             = require('./config/db');
+const express            = require('express');
+const mongoose           = require('mongoose');
+const bodyParser         = require('body-parser');
+const db                 = require('./config/db');
+const app                = express();
+
+// OauthServer
+const OAuth2Server       = require('oauth2-server');
+const AccessDeniedError  = require('oauth2-server/lib/errors/access-denied-error');
+const Request            = OAuth2Server.Request;
+const Response           = OAuth2Server.Response;
+
+const oauth = new OAuth2Server({
+  model: require('./app/models/model.js'),
+  grants: ['password'],
+  debug: true
+});
 
 const port = 8000;
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// MongoDB Connection using mongoose
 mongoose.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
@@ -15,3 +31,16 @@ mongoose.connect(db.url, (err, database) => {
     console.log('We are live on ' + port);
   });
 })
+
+
+//    let request = new Request({
+//        method: 'POST',
+//        query: {},
+//        headers: {
+//           Authorization: 'Bearer ﻿422e5d1867c2f6ca6134f9553b4b339fda98f9ff'
+//        }
+//    });
+
+
+
+
